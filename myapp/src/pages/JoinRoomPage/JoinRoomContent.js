72a8870eb6df9e5c34e891cce7f3a6c390df1f07
ms-online <27732357@qq.com>
2021-12-08 17:19:08 +0800

    lesson-15

--- myapp/src/pages/JoinRoomPage/JoinRoomContent.js
+++ myapp/src/pages/JoinRoomPage/JoinRoomContent.js
@@ -6,14 +6,23 @@ import { setConnectOnlyWithAudio } from '../../store/actions';
 import ErrorMessage from './ErrorMessage';
 import JoinRoomButtons from './JoinRoomButtons';
 import { getRoomExists } from '../../utils/api';
+import { useNavigate } from 'react-router-dom';
 const JoinRoomContent = (props) => {
   const { isRoomHost, setConnectOnlyWithAudio, connectOnlyWithAudio } = props;
   const [roomIdValue, setRoomIdValue] = useState('');
   const [nameValue, setNameValue] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
-
+  const navigate = useNavigate();
   //加入房间
   const handleJoinRoom = async () => {
+    if (isRoomHost) {
+      createRoom();
+    } else {
+      await joinRoom();
+    }
+  };
+
+  const joinRoom = async () => {
     const responseMessage = await getRoomExists(roomIdValue);
 
     const { roomExists, full } = responseMessage;
@@ -23,11 +32,17 @@ const JoinRoomContent = (props) => {
         setErrorMessage('会议房间人数已满，请稍后再试！');
       } else {
         //进入房间
+        navigate('/room');
       }
     } else {
       setErrorMessage('会议房间不存在，请验证你的ID是否正确！');
     }
   };
+
+  const createRoom = () => {
+    navigate('/room');
+  };
+
   return (
     <>
       <JoinRoomInputs
