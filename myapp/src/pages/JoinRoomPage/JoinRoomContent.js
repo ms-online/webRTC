8ccf6dfc434f96d64d3321ed84d70c6344798363
ms-online <27732357@qq.com>
2021-12-08 17:04:11 +0800

    lesson-14

--- myapp/src/pages/JoinRoomPage/JoinRoomContent.js
+++ myapp/src/pages/JoinRoomPage/JoinRoomContent.js
@@ -5,15 +5,28 @@ import OnlyWithAudioCheckbox from './OnlyWithAudioCheckbox';
 import { setConnectOnlyWithAudio } from '../../store/actions';
 import ErrorMessage from './ErrorMessage';
 import JoinRoomButtons from './JoinRoomButtons';
+import { getRoomExists } from '../../utils/api';
 const JoinRoomContent = (props) => {
   const { isRoomHost, setConnectOnlyWithAudio, connectOnlyWithAudio } = props;
   const [roomIdValue, setRoomIdValue] = useState('');
   const [nameValue, setNameValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
 
   //加入房间
-  const handleJoinRoom = () => {
-    //加入房间
-    console.log('成功加入房间');
+  const handleJoinRoom = async () => {
+    const responseMessage = await getRoomExists(roomIdValue);
+
+    const { roomExists, full } = responseMessage;
+
+    if (roomExists) {
+      if (full) {
+        setErrorMessage('会议房间人数已满，请稍后再试！');
+      } else {
+        //进入房间
+      }
+    } else {
+      setErrorMessage('会议房间不存在，请验证你的ID是否正确！');
+    }
   };
   return (
     <>
@@ -28,7 +41,7 @@ const JoinRoomContent = (props) => {
         connectOnlyWithAudio={connectOnlyWithAudio}
         setConnectOnlyWithAudio={setConnectOnlyWithAudio}
       />
-      <ErrorMessage errorMessage='会议ID不正确！' />
+      <ErrorMessage errorMessage={errorMessage} />
       <JoinRoomButtons
         isRoomHost={isRoomHost}
         handleJoinRoom={handleJoinRoom}
--- server/server.js
+++ server/server.js
@@ -13,6 +13,30 @@ const server = http.createServer(app);
 //cors包解决跨域访问错误
 app.use(cors());
 
+//初始化房间和用户
+let connectedUsers = [];
+let rooms = [];
+
+//创建路由验证房间是否存在
+app.get('/api/room-exists/:roomId', (req, res) => {
+  const { roomId } = req.params;
+  const room = rooms.find((room) => room.id === roomId);
+
+  if (room) {
+    //房间存在
+    if (room.connectedUsers.length > 3) {
+      //房间人数已满
+      return res.send({ roomExists: true, full: true });
+    } else {
+      //房间可以加入
+      return res.send({ roomExists: true, full: false });
+    }
+  } else {
+    //房间不存在
+    return res.send({ roomExists: false });
+  }
+});
+
 // 传递server对象，初始化一个io实例
 const io = require('socket.io')(server, {
   cors: {
