79ef3ceaa72b1f22c383818e391840b281e461e6
ms-online <27732357@qq.com>
2021-12-13 17:44:58 +0800

    lesson-43

--- myapp/src/pages/RoomPage/VideoSection/SwitchToScreenSharingButton.js
+++ myapp/src/pages/RoomPage/VideoSection/SwitchToScreenSharingButton.js
@@ -1,19 +1,50 @@
 import React, { useState } from 'react';
 import SwitchImg from '../../../resources/images/switchToScreenSharing.svg';
+import LocalScreenSharingPreview from './LocalScreenSharingPreview';
+
+const constrains = {
+  audio: false,
+  video: true,
+};
+
 const SwitchToScreenSharingButton = () => {
   const [isScreenSharingActive, setIsScreenSharingActive] = useState(false);
+  const [screenSharingStream, setScreenSharingStream] = useState(null);
+  const handleScreenShareToggle = async () => {
+    if (!isScreenSharingActive) {
+      let stream = null;
+      try {
+        //获取本地要共享的媒体资源
+        stream = await navigator.mediaDevices.getDisplayMedia(constrains);
+      } catch (error) {
+        console.log('获取共享屏幕的媒体流失败');
+      }
+      if (stream) {
+        setScreenSharingStream(stream);
+        setIsScreenSharingActive(true);
+      }
+    } else {
+      setIsScreenSharingActive(false);
+      //停止共享屏幕
+      screenSharingStream.getTracks().forEach((track) => track.stop());
+      setScreenSharingStream(null);
+    }
 
-  const handleScreenShareToggle = () => {
-    setIsScreenSharingActive(!isScreenSharingActive);
+    // setIsScreenSharingActive(!isScreenSharingActive);
   };
   return (
-    <div className='video_button_container'>
-      <img
-        src={SwitchImg}
-        onClick={handleScreenShareToggle}
-        className='video_button_image'
-      />
-    </div>
+    <>
+      <div className='video_button_container'>
+        <img
+          src={SwitchImg}
+          onClick={handleScreenShareToggle}
+          className='video_button_image'
+        />
+      </div>
+      {isScreenSharingActive && (
+        <LocalScreenSharingPreview stream={screenSharingStream} />
+      )}
+    </>
   );
 };
 
