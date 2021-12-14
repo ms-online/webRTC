4c5f05bb667686e0ad6569893b021840416827ab
ms-online <27732357@qq.com>
2021-12-14 15:13:06 +0800

    lesson-45

--- myapp/src/pages/RoomPage/ChatSection/ChatSection.js
+++ myapp/src/pages/RoomPage/ChatSection/ChatSection.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import ChatLabel from './ChatLabel';
+import Messages from './Messages';
 const ChatSection = () => {
-  return <div className='chat_section_container'>这是聊天室区域</div>;
+  return (
+    <div className='chat_section_container'>
+      <ChatLabel />
+      <Messages />
+    </div>
+  );
 };
 
 export default ChatSection;
